f5acc3d7757b014199cee41efefd3fb5fa72df53
Stolbov Nikita <nikita-stolbov-99@mail.ru>
2025-01-14 15:30:02 +0300

    Changing styles

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -4,21 +4,48 @@ import { useState } from "react";
 
 function JournalForm({onSubmit}) {
 
-  const [inputData, setInputData] = useState("");
+  const [formValidState, setFormValidState] = useState({
+    title: true,
+    text: true,
+    date: true
+  })
 
   const addJournalItem = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const formProps = Object.fromEntries(formData);
+    let isValid = true;
+    if(!formProps.title?.trim().length){
+      setFormValidState(state => ({...state, title: false}));
+      isValid = false;
+    } else {
+      setFormValidState(state => ({...state, title: true}));
+    }
+    if(!formProps.text?.trim().length){
+      setFormValidState(state => ({...state, text: false}));
+      isValid = false;
+    } else {
+      setFormValidState(state => ({...state, text: true}));
+    }
+    if(!formProps.date){
+      setFormValidState(state => ({...state, date: false}));
+      isValid = false;
+    } else {
+      setFormValidState(state => ({...state, date: true}));
+    }
+
+    if(!isValid) {
+      return;
+    }
     onSubmit(formProps);     // Здесь вызываем ф-ю
   }
 
   return (
     <form className="journal-form" onSubmit={addJournalItem}>
-      <input type="text" name="title" />
-      <input type="date" name="date" />
+      <input type="text" name="title" style={{border: formValidState.title ? undefined : "1px solid red"}}/>
+      <input type="date" name="date" style={{border: formValidState.date ? undefined : "1px solid red"}}/>
       <input type="text" name="tag" />
-      <textarea name="text" cols="30" rows="10"></textarea>
+      <textarea name="text" cols="30" rows="10" style={{border: formValidState.text ? undefined : "1px solid red"}}></textarea>
       <Button text="Сохранить"/>
     </form>
   )
